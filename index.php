3a36c21ff7801e9c8e62017c7ff1f3daa7f9191c
Andreani Operaciones <aaros@andreani.com>
2018-10-19 12:10:38 -0300

    index

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<title>
+<?php echo ''.gethostname() . "&nbsp;Server UP!" ?>
+</title>
+<style>
+.html, body {
+    color: #878b90;
+    font-family: "Helvetica Neue",Helvetica,Arial,sans-serif;
+    font-style: normal;
+    font-variant: normal;
+    background-color:#272b30;
+}
+</style>
+</head>
+<body>
+
+
+<?php
+echo "Server&nbsp; <span style=color:red; >" . gethostname() ."</span>&nbsp;is <span style=color:#58FA58;>UP!</span>";
+?>
+<body>
+</html>
